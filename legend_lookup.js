62c44c2d55e23d96c23ce92ddcf8bf4d662711e6
Matthew Geiger <mgeiger@unca.edu>
2017-02-20 13:11:51 -0500

    add legend_lookup.js

---
+++ legend_lookup.js
@@ -0,0 +1,23 @@
+
+
+const legendConfig = {
+
+  'landcover-2010-NALCMS': {
+    '1': 'Temperate or sub-polar needleleaf forest',
+    '3': 'Tropical or sub-tropical broadleaf evergreen forest',
+    '5': 'Temperate or sub-polar broadleaf deciduous forest',
+    '6': 'Mixed forest',
+    '7': 'Tropical or sub-tropical shrubland',
+    '8': 'Temperate or sub-polar shrubland',
+    '9': 'Tropical or sub-tropical grassland',
+    '10': 'Temperate or sub-polar grassland',
+    '13': 'Sub-polar or polar barren-lichen-moss',
+    '14': 'Wetland',
+    '15': 'Cropland',
+    '16': 'Barren land',
+    '17': 'Urban and built-up',
+    '18': 'Water',
+    '19': 'Snow and ice'
+  }
+
+}
